5348ffdd03b6a86bd22a3aca2ceeeae3589d66fb
vadymshpak09 <167808356+vadymshpak09@users.noreply.github.com>
2025-04-24 15:18:02 +0200

    fix

--- src/utils/isPathActive.ts
+++ src/utils/isPathActive.ts
@@ -2,6 +2,7 @@ export function isPathActive(basePath: string, currentPath: string): boolean {
 	const cleaned = basePath.replace(/\/$/, '')
 	console.log(basePath)
 	console.log(cleaned)
+	console.log(currentPath)
 	if (cleaned === '') {
 		return currentPath === '/'
 	}
