ca731507e6d0ea702b11f91b641cd9e906775307
vadymshpak09 <167808356+vadymshpak09@users.noreply.github.com>
2025-04-24 16:02:42 +0200

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { Metadata } from 'next'
 
 import { NO_INDEX_PAGE } from '@/constants/seo.constants'
--- src/components/dashboard/layout/DashboardLayout.tsx
+++ src/components/dashboard/layout/DashboardLayout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { PropsWithChildren } from 'react'
 
 import { Header } from '../Header/Header'
